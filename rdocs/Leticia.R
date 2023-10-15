ac0bcf743cc0543a439e498010b64dc74c2e5ae4
letrlima <leticiairdelima@gmail.com>
2023-10-15 20:48:26 -0300

    Codigos dos graficos analise 1

--- rdocs/Leticia.R
+++ rdocs/Leticia.R
@@ -1,6 +1,7 @@
 vendas <- read_csv("banco/vendas.csv")
 devolução <- read_csv("banco/devolução.csv")
 caminho_Leticia <- "resultados"
+teste <- "teste"
 
 library(tidyverse)
 library(dplyr)
@@ -29,16 +30,21 @@ colnames(vendas)[11]<-'Cor'
 colnames(vendas)[12]<-'Tamanho'
 colnames(vendas)[13]<-'ID_unica'
 
+## Arrumando o layout das datas
+vendas <- vendas %>% 
+  mutate(`Data Venda` = mdy(vendas$`Data Venda`))
+
+## Salvando a paleta de cores da ESTAT
 cores_estat <- c("#A11D21", "#003366", "#CC9900", "#663333", "#FF6600", "#CC9966", "#999966", "#006606", "#008091", "#041835", "#666666")
 theme_estat <- function(...){
   theme <- ggplot2::theme_bw() +
     ggplot2:: theme(
       axis.title.y = ggplot2::element_text( colour = "black ",
-                                                size = 12),
+                                            size = 12),
       axis.title.x = ggplot2::element_text( colour = "black ",
-                                                size = 12),
+                                            size = 12),
       axis.text = ggplot2::element_text( colour = " black", size
-                                            = 9.5),
+                                         = 9.5),
       panel.border = ggplot2::element_blank(),
       axis.line = ggplot2::element_line( colour = " black"),
       legend.position = "top",
@@ -46,8 +52,8 @@ theme_estat <- function(...){
     )
   return (
     list( theme,
-      scale_fill_manual( values = cores_estat ),
-      scale_colour_manual( values = cores_estat )))}
+          scale_fill_manual( values = cores_estat ),
+          scale_colour_manual( values = cores_estat )))}
 
 
 #### Retirando as linhas com ID unicas que estão duplicadas
@@ -57,102 +63,54 @@ vendas <- vendas[!duplicated(vendas$ID_unica),]
 
 ### Faturamento anual por categoria
 
-vendas <- vendas %>% 
-  mutate(`Data Venda` = mdy(vendas$`Data Venda`))
-
-##Tabela porcentagem faturamento por categoria
-tabelax <- vendas %>% 
-  filter(!is.na(Preco)) %>% 
-  filter(!is.na(Categoria)) %>% 
+## Gráfico de colunas do faturamento anual por categoria
+tabela1 <- vendas %>%
+  filter(!is.na(Preco)) %>%
+  filter(!is.na(Categoria)) %>%
   select(Preco, Categoria) %>% 
   group_by(Categoria) %>% 
-  summarise(Preco = sum(Preco)) %>%
-  mutate(Freq = Preco / sum(Preco))
-tabelax
+  summarise(Preco = sum(Preco)) %>% 
+  mutate(freq = Preco / sum(Preco)) %>%
+  mutate(
+    freq = scales::percent(freq, scale = 100, accuracy = 0.01),
+    freq = gsub("\\.", ",", freq) %>% paste( sep = ""),
+    label = str_c(Preco, " (", freq, ")") %>% str_squish()
+  )
 
-#Ordenando em ordem decrescente
-tabelax <- tabelax %>% arrange(desc(Preco))
-tabelax
+ggplot(tabela1) +
+  aes(x = fct_reorder(Categoria, Preco, .desc = TRUE), y = Preco, label = label) +
+  geom_bar(stat = "identity", fill = c("#003366","#A11D21", "#CC9900"), width = 0.7) +
+  geom_text(
+    position = position_dodge(width = 0.9),
+    vjust = -0.5,
+    size = 3
+  ) + 
+  labs(x = "Categoria", y = "Faturamento anual")
+ggsave(filename = file.path(caminho_Leticia, "faturamento-colunas-freq.pdf"), width = 158, height = 93, units = "mm")
 
-# Configurar a formatação de porcentagem com duas casas decimais
-percent_format <- scales::percent_format(accuracy = 0.01)
 
-## Gráfico de barras do Faturamento anual por Categoria
-ggplot(tabelax, aes(fct_reorder(Categoria, -Preco), Preco, label = percent_format(Freq)))  +
-  geom_bar(stat = "identity", fill = c("#A11D21", "#003366", "#CC9900")) +
-  xlab("Categoria")+
-  ylab("Faturamento Anual")+
-  ggtitle("Gráfico de barras do faturamento anual por categoria") + 
-  geom_text(position = position_dodge(width = .9),
-            vjust = -0.5, hjust = .5,
-            size = 3)  +
-  theme_estat() +
-  theme(legend.title = element_blank())
-ggsave(filename = file.path(caminho_Leticia, "barras_freq_faturamento.pdf"), width = 158, height = 93, units = "mm")
+## Gráfico de setores do faturamento anual por categoria
+tabela2 <- vendas %>% 
+  filter(!is.na(Preco)) %>%
+  filter(!is.na(Categoria)) %>%
+  select(Preco, Categoria) %>% 
+  group_by(Categoria) %>% 
+  summarise(Preco = sum(Preco)) %>% 
+  mutate(freq = round(100*(Preco / sum(Preco)), 2)) %>%
+  arrange(desc(Categoria)) %>%
+  mutate(posicao = cumsum(freq) - 0.5*freq)
 
-## Gráfico de Setores do Faturamento anual por Categoria
-ggplot(tabelax, aes(x = "", y = Preco, fill = Categoria)) +
-  geom_bar(stat = "identity", width = 1) +
+ggplot(tabela2) +
+  aes(x = factor(""), y = freq , fill = factor(Categoria)) +
+  geom_bar(width = 1, stat = "identity") +
   coord_polar(theta = "y") +
-  xlab("") +
-  ylab("") +
-  ggtitle("Gráfico de Setores do Faturamento Anual por Categoria") +
-  scale_fill_manual(values = c("#A11D21", "#003366", "#CC9900")) +
-  geom_text(aes(x = 1.8, label = scales::percent(Freq)), position = position_stack(vjust = 0.5))+
+  geom_text(
+    aes(x = 1.8, y = posicao, label = paste0(freq, "%")),
+    color = "black"
+  ) +
   theme_void() +
   theme(legend.position = "top") +
-  scale_fill_manual(values = cores_estat, name = 'Categorias')
-ggsave(filename = file.path(caminho_Leticia, "setores_freq_faturamento.pdf"), width = 158, height = 93, units = "mm")
-
-##### DAR UM JEITO DE COLOCAR COM DUAS CASAS DECIMAIS 
+  scale_fill_manual(values = cores_estat, name = 'Categoria')
+ggsave(filename = file.path(caminho_Leticia, "faturamento-setores-freq.pdf"), width = 158, height = 93, units = "mm")
 
 
-## Gráfico de barras do faturamento por categorias (separado)
-tabelax %>% 
-  filter(Categoria == "Moda Feminina") %>% 
-  ggplot(aes(fct_reorder(Categoria, -Preco), Preco, label = percent_format(Freq)))  +
-  geom_bar(stat = "identity", fill = "#A11D21") +
-  xlab("Categoria")+
-  ylab("Faturamento Anual")+
-  ggtitle("Gráfico de barras do faturamento anual por categoria") + 
-  geom_text(position = position_dodge(width = .9),
-            vjust = -0.5, hjust = .5,
-            size = 3)  +
-  theme_estat() +
-  theme(legend.title = element_blank())
-
-tabelax %>% 
-  filter(Categoria == "Moda Masculina") %>% 
-  ggplot(aes(fct_reorder(Categoria, -Preco), Preco, label = percent_format(Freq)))  +
-  geom_bar(stat = "identity", fill = "#A11D21") +
-  xlab("Categoria")+
-  ylab("Faturamento Anual")+
-  ggtitle("Gráfico de barras do faturamento anual por categoria") + 
-  geom_text(position = position_dodge(width = .9),
-            vjust = -0.5, hjust = .5,
-            size = 3)  +
-  theme_estat() +
-  theme(legend.title = element_blank())
-
-tabelax %>% 
-  filter(Categoria == "Moda Infantil") %>% 
-  ggplot(aes(fct_reorder(Categoria, -Preco), Preco, label = percent_format(Freq)))  +
-  geom_bar(stat = "identity", fill = "#A11D21") +
-  xlab("Categoria")+
-  ylab("Faturamento Anual")+
-  ggtitle("Gráfico de barras do faturamento anual por categoria") + 
-  geom_text(position = position_dodge(width = .9),
-            vjust = -0.5, hjust = .5,
-            size = 3)  +
-  theme_estat() +
-  theme(legend.title = element_blank())
-
-
-tabelay <- vendas %>% 
-  filter(!is.na(Preco)) %>% 
-  filter(!is.na(Categoria)) %>% 
-  filter(Categoria == "Moda Feminina") %>% 
-  select(Preco, Categoria, Nome_Produto) %>% 
-  group_by(Nome_Produto) %>% 
-  summarise(Preco = sum(Preco)) %>%
-  mutate(Freq = Preco / sum(Preco))
